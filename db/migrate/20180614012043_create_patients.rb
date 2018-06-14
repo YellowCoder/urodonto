b2f225d1fa5e558def2c3b42346d2cb963178f0c
Adriano Tadao <adrianotadao@gmail.com>
2018-06-13 22:40:08 -0300

    Saving Patients

---
+++ db/migrate/20180614012043_create_patients.rb
@@ -0,0 +1,21 @@
+class CreatePatients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :patients do |t|
+      t.boolean :active
+      t.boolean :temporary
+
+      t.string :name, null: false
+      t.string :birthday
+      t.string :sex
+
+      t.string :phone
+      t.string :cell_phone
+      t.string :email
+      
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip_code
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_14_003056) do
+ActiveRecord::Schema.define(version: 2018_06_14_012043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "patients", force: :cascade do |t|
+    t.boolean "active"
+    t.boolean "temporary"
+    t.string "name"
+    t.string "birthday"
+    t.string "sex"
+    t.string "phone"
+    t.string "cell_phone"
+    t.string "email"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
