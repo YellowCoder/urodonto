996546380a2cb5a1f5d49f1d395ad9a0f86e40fd
Adriano Tadao <adrianotadao@gmail.com>
2018-06-13 22:17:42 -0300

    Creating the layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
-  
+  layout :current_layout
+
   before_action :authenticate_user!
 
   def index
+  end
+
+  private
+
+  def current_layout
+    user_signed_in? ? 'application' : 'public'
   end
 end
