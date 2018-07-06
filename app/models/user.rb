afcac617b9a484fed7fccf5957fbff2de51601fa
Adriano Tadao <adrianotadao@gmail.com>
2018-07-06 18:49:25 -0300

    Improvements on layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, 
+  devise :database_authenticatable, :rememberable, 
     :trackable, :validatable, :registerable
 end
