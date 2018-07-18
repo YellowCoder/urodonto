9b35669371f99fd9acf715fb46990fab0b6d2d66
Adriano Tadao <adrianotadao@gmail.com>
2018-07-17 23:32:37 -0300

    Add colors to show the payment status

--- app/decorators/appointment_decorator.rb
+++ app/decorators/appointment_decorator.rb
@@ -2,17 +2,33 @@ class AppointmentDecorator < Draper::Decorator
   delegate_all
   
   def payment_status
-    case
-    when object.financial_record.blank? && object.chargeable? && object.delayed? then 'Atrasado'
-    when object.financial_record.blank? && object.chargeable? then 'Aberto'
-    when object.financial_record.blank? && !object.chargeable? then 'Gratuito'
-    else
-      'Pago'
-    end
+    return :free unless object.chargeable?
+    return :overdue if object.delayed? && object.financial_record.blank?
+    return :not_paid if object.chargeable? && object.financial_record.blank?
+    :paid
   end
 
   def payment_link_or_label
-    return payment_status unless object.financial_record
-    h.link_to payment_status, h.financial_record_path(object.financial_record)
+    return statuses[payment_status][:label] if object.financial_record.blank?
+    h.link_to statuses[payment_status][:label], h.financial_record_path(object.financial_record)
+  end
+
+  def status_humanized
+    h.t("activerecord.attributes.appointment.statuses.#{ object.status }")
+  end
+
+  def payment_color
+    statuses[payment_status][:color]
+  end
+
+  private
+
+  def statuses
+    {
+      paid: { label: 'Pago', color: 'green' },
+      overdue: { label: 'Atrasado', color: 'red' },
+      not_paid: { label: 'Aberto', color: 'yellow' },
+      free: { label: 'Gratuito', color: 'gray' }
+    }
   end
 end
